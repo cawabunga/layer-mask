a99e6ff1d79d3e142450f29ca885da81b7073404
Emil Ibatullin <emil.ibatullin@gmail.com>
2017-10-31 18:40:56 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+const webpack = require('webpack');
+const path = require('path');
+const _ = require('lodash');
+const pkg = require('./package.json');
+const libraryName = pkg.name;
+
+module.exports = {
+  entry: {
+    [libraryName]: `${__dirname}/index.js`
+  },
+  output: {
+    filename: 'dist/[name].js',
+    library: _.camelCase(libraryName),
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015'
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'raw-loader'
+      }
+    ],
+  }
+};
